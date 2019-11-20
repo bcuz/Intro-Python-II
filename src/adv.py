66df4e3aaabacaddfea16eac62349f85d91b5263
adam <bcuz@users.noreply.github.com>
2019-11-19 22:25:33 -0500

    change to tab

--- src/adv.py
+++ src/adv.py
@@ -4,20 +4,20 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+	'outside':  Room("Outside Cave Entrance",
+					 "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+	'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+	'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+	'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+	'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -58,21 +58,21 @@ directions = {
 }
 
 while True:
-    print('----------------------------');
-    print(f'Room: {player.currentRoom.name}');
-    print(f'Description: {player.currentRoom.description}\n');
-    response = input("Move north, south, east, or west (type n, s, e, or w). Or q to quit: ")
+	print('----------------------------');
+	print(f'Room: {player.currentRoom.name}');
+	print(f'Description: {player.currentRoom.description}\n');
+	response = input("Move north, south, east, or west (type n, s, e, or w). Or q to quit: ")
 
-    if response in directions.keys():
-        checkDirection = getattr(player.currentRoom, directions[response])
+	if response in directions.keys():
+		checkDirection = getattr(player.currentRoom, directions[response])
 
-        if checkDirection != None:
-          # change current room
-            player.currentRoom = getattr(player.currentRoom, directions[response])
-        else:
-            input('Cant move in this direction. Press enter to try again ');
-    elif response == 'q':
-        print('Goodbye');
-        break
-    else:
-      input('Error, please enter valid command. Press enter to try again ');
+		if checkDirection != None:
+		  # change current room
+			player.currentRoom = getattr(player.currentRoom, directions[response])
+		else:
+			input('Cant move in this direction. Press enter to try again ');
+	elif response == 'q':
+		print('Goodbye');
+		break
+	else:
+	  input('Error, please enter valid command. Press enter to try again ');
