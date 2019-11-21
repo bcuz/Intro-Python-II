57c57d28041ceb290d18213152efd0660f662acb
adam <bcuz@users.noreply.github.com>
2019-11-20 23:51:04 -0500

    add items to rooms

--- src/room.py
+++ src/room.py
@@ -2,9 +2,10 @@
 # description attributes.
 
 class Room:
-	def __init__(self, name, description, n_to=None, s_to=None, e_to=None, w_to=None):
+	def __init__(self, name, description, items=[], n_to=None, s_to=None, e_to=None, w_to=None):
 		self.name = name
 		self.description = description
+		self.items = items
 		self.n_to = n_to
 		self.s_to = s_to
 		self.e_to = e_to
